bb9a3e52eedbded0cc68008385bb909d3aeba15c
errakisiham <siham95er@gmail.com>
2023-06-16 18:59:58 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main- prints the alphabet
+ * Return : 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A' ; c <= 'Z' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
