e6d144910d31af08efaf6287093b0372fac56723
errakisiham <siham95er@gmail.com>
2023-06-16 19:59:58 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- p rints all single number of bas 10
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	for (num =0; < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
