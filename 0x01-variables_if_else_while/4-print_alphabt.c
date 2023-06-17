7076490dd5c4bf2607d10a4a41920d68a0961897
errakisiham <siham95er@gmail.com>
2023-06-16 19:49:55 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main- Prints the alphabet in lowercase
- *
- * Return:0 if success 
+ * Return:0if success 
  */
-int main(void){
+int main(void)
+{
 	int c;
 
 	for (c = 'a' ; c <= 'z' ; c++)
