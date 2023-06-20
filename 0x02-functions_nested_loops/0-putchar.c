b3d73029819ac61f7c2d6e218972c5651444d4e3
errakisiham <siham95er@gmail.com>
2023-06-20 10:36:32 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "_putchar.h)
+
+int main(void)
+{
+	_putchar('_);
+	_puthcar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
