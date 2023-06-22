826108d8ce01b671a2359a3edbbe40bb8b9b92b3
errakisiham <siham95er@gmail.com>
2023-06-22 16:40:47 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * _isupper - check
+ * @l, number
+ * Return: 1 for upper letter or 0 for any else
+ */
+
+int_isupper(int x)
+{
+	if (x >= 65 && x <=90)
+	{
+		return (1);
+	}
+	return (0);
+}
