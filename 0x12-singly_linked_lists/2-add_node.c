664fcf451e5ef46b042b7c0e622d4b11d0138cf5
sihamerraki <siham95er@gmail.com>
2023-07-28 07:08:39 -0700

    ..

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include "lists.h"
-
 /**
- * list_len - returns the number of elements in a linked list
+ * list_len - returns the number in a linked list
  * @h: pointer to the list_t list
  *
  * Return: number of elements in h
