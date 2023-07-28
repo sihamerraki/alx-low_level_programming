3a20a22a9c8b5fe18393126c3198d7d3b4e655cd
sihamerraki <siham95er@gmail.com>
2023-07-28 06:54:53 -0700

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h);
 {
-	size_t n = 0;
+	size_t s = 0;
 	while (h)
 {
 if (!h->str)
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,7 +6,6 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  */
 typedef struct list_s
@@ -21,6 +20,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-
 #endif
