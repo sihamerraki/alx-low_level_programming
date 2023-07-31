047396d8917ba277ef6bfca3cff3b1a8ff18ebb8
sihamerraki <siham95er@gmail.com>
2023-07-31 14:29:34 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,12 +6,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t number = 0;
+	size_t n = 0;
 	while (h)
 	{
 		printf("%d\n",(*h).n);
-		number++;
+		n++;
 		h = (*h).next;
 	}
-	return (number);
+	return (n);
 }
