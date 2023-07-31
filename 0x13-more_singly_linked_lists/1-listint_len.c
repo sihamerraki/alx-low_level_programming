d9179e2b31232d44635e80bdf108a78a0936f538
sihamerraki <siham95er@gmail.com>
2023-07-31 16:06:43 -0700

    1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,7 +4,7 @@
  * @h: linked list of listint_to traverse
  * Return: the number of nodes
  */
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
 	size_t n=0;
 	while (h)
