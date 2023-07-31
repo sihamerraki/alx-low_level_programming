dbf1cbcb91a23d4f256fe8acfd5c6929d55dd328
sihamerraki <siham95er@gmail.com>
2023-07-31 16:22:24 -0700

    1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,11 +7,11 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t n = 0;
+	size_t num = 0;
 	while (h)
 	{
-		n++;
-		h = h->next;
+		num++;
+		h = (*h).next;
 	}
-	return (n);
+	return (num);
 }
