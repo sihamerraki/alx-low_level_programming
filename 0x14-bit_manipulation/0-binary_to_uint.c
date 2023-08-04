fafa1e577275516a64f22d03511037b150c47dbf
sihamerraki <siham95er@gmail.com>
2023-08-03 17:54:45 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
@@ -8,14 +7,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int new_val = 0;
+	unsigned int dec_val = 0;
 	if (!b)
 		return (0);
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		new_val = 2 * new_val + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (new_val);
+	return (dec_val);
 }
+
