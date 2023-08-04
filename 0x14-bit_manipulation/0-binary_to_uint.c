a4ea859cd28d3137be5ed122fc69c86edd2fe4be
sihamerraki <siham95er@gmail.com>
2023-08-04 05:51:02 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,24 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * binary_to_uint - Convert a binary number to an unsigned int.
- * @b: A pointer to a string containing the binary number.
- * Return: The converted number, or 0 if invalid input.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-    if (b == NULL)
-        return 0;
+	if (!b)
+		return (0);
 
-    for (int i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] == '0')
-        {
-            result <<= 1;
-        }
-        else if (b[i] == '1')
-        {
-            result = (result << 1) | 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-    return result;
+	return (dec_val);
 }
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,1 +1,1 @@
-0x14. C - Bit ManipULATION
+0x14. C - Bit Manipulation
